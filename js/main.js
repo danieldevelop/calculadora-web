4f6cea7ef65270e6499b20d82c84205ef8a3a524
Daniel Gomez <51731637+danieldevelop@users.noreply.github.com>
2026-02-15 15:15:25 -0300

    add calcular y limpiar input cuando hacemos click en el boton C

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 "use strict";
 
+import { clearDisplay } from './utils.js';
+
 let displayValue = '';
 const display = document.getElementById('display');
 
 const appendToDisplay = (value) => {
     displayValue += value;
     display.value = displayValue;
+}
+
+const clearDisplay = () => {
+    displayValue = '';
+    display.value = displayValue;
+}
+
+const calculate = () => {
+    try {
+        displayValue = eval(displayValue);
+        display.value = displayValue;
+    } catch (error) {
+        display.value = 'Error';
+    }
 }
